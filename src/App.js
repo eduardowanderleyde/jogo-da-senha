fcdbb4df37e86b4f16299ea5ba4ee711544dba90
eduardowanderleyde <wanderley.eduardo@gmail.com>
2022-11-14 11:00:36 -0300

    “primeiro“

--- src/App.js
+++ src/App.js
@@ -1,83 +1,116 @@
-import './App.css';
-import React, { useState, useEffect } from "react";
-import { comparar } from "./componentes/Comparar.js";
-import { resultadoA } from './componentes/Comparar.js';
-import { resultadoB } from './componentes/Comparar.js';
+import "./style.css";
+import { useState, useEffect } from "react";
 
-
-export function App() {
-
-  const [senha, setSenha] = useState("");
-  const [tentativa, setTentativa] = useState("");
-  const [senhaOficial, setsenhaOficial] = useState("");
-  const [resultadosSenha, setResultados] = useState([]);
-  const [resultadosA, setResultadosA] = useState([]);
-  const [resultadosB, setResultadosB] = useState([]);
-
-
-  function salvarSenha() {
-    setsenhaOficial(senha);
-    setSenha("");
-    //console.log("sajkux");
+const hiddenNumber = () => {
+  const generateHiddenNumber = Math.floor(Math.random() * 9000 + 1000);
+  if (checkHiddenUnique(generateHiddenNumber)) {
+    return generateHiddenNumber;
+  } else {
+    return hiddenNumber;
   }
+};
 
-  function chamaComparar() {
-    comparar({ tentativa }, { senhaOficial });
-    if(resultadoA>0){
-      resultadosSenha.push(tentativa);
-      resultadosA.push(resultadoA);
-      resultadosB.push(resultadoB);
+const checkHiddenUnique = (givenNumber) => {
+  return !/(.).*?\1/.test(givenNumber);
+};
+
+//COMMENT
+const testInput = (hiddenNumber, inputData) => {
+  let bull = 0;
+  let cow = 0;
+  for (let i = 0; i < hiddenNumber.length; i++) {
+    if (
+      inputData.includes(hiddenNumber[i]) &&
+      hiddenNumber[i] === inputData[i]
+    ) {
+      bull++;
+
+      if (bull === 4) {
+        return alert("ACERTOU MIZERAVI!");
+      }
+    } else if (inputData.includes(hiddenNumber[i])) {
+      cow++;
+    }
+  }
+  return {
+    inputData: inputData,
+    bull: bull,
+    cow: cow,
+  };
+};
+
+function App() {
+  useEffect(() => {
+    setgenerated(hiddenNumber());
+  }, []);
+
+  const [generatedNumber, setgenerated] = useState(0);
+  const [checkBull, setcheckBull] = useState(0);
+  const [checkCow, setcheckCow] = useState(0);
+  const [inputData, setData] = useState("");
+  const [historyInput, sethistoryInput] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (inputData === "") {
+      return alert("numero");
+    } else if (inputData.length < 4) {
+      return alert("ta faltando número!");
+    }
+
+    const testresult = testInput(generatedNumber.toString(), inputData);
+    setcheckBull(testresult.bull);
+    setcheckCow(testresult.cow);
+
+    if (historyInput.length === 0) {
+      sethistoryInput([testresult]);
+    } else {
+      sethistoryInput((history) => [...history, testresult]);
     }
   };
 
+  const handleInput = (e) => {
+    console.log(e.target.value);
+    setData(e.target.value);
+  };
+
+  //JSX
   return (
-
-    <div className="App">
-
-      <div className='container'>
-        <h1>JOGO DA SENHA</h1>
-
-        <div>
-          <h2>Número secreto</h2>
-          <input
-            type='number'
-            placeholder='Digite a senha aqui'
-            value={senha}
-            onChange={(evt) => setSenha(evt.target.value)}>
-          </input>
-          <button onClick={salvarSenha}>Salvar senha</button>
+    <div className="page-wrapper">
+      <div className="game-wrapper">
+        <div className="header-wrapper">
+          <h1>Touros & Vacas </h1>
+          <p></p>
         </div>
-
-        <div>
-          <h2>Tentativas</h2>
-          <input
-            type='number'
-            placeholder='Tente adivinhar a senha'
-            value={tentativa}
-            onChange={(evt) => setTentativa(evt.target.value)}>
-          </input>
-          <button onClick={chamaComparar}>OK</button>
+        <div className="input-wrapper">
+          <form action="">
+            <input type="text" required maxLength="4" onChange={handleInput} />
+            <button onClick={handleSubmit}>Tentar!</button>
+          </form>
         </div>
-
-        <table border={1}>
-          <tr>
-            <th>Tentativas</th>
-            <th>Senhas</th>
-            <th>Resultados</th>
-          </tr>
-          {resultadosSenha.map((senha, index) => (
-            <tr key={index}>
-              <td>{index + 1}</td>
-              <td>{senha}</td>
-              <td>{resultadosA[index]}A{resultadosB[index]}B</td>
-            </tr>
-          ))}
-        </table>
-
+        <div className="display-wrapper">
+          <div className="display-result">
+            <h3>
+              Touros:{checkBull} Vacas:{checkCow}
+            </h3>
+          </div>
+          <div className="display-input">
+            <ul>
+              {historyInput.map((history, index) => {
+                return (
+                  <li key={index}>
+                    {history.inputData}
+                    {/* {history.bull} {history.cow} */}
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
-
   );
 }
 
 export default App;
+export { hiddenNumber, checkHiddenUnique, testInput };
